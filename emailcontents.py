95dc53fe0e2a54b57d2ac31ac66056c153919c7e
Abdu865 <abdullahhijazi3@gmail.com>
2022-01-31 00:54:21 -0500

    updated sender to use the file emailcontents.py to make HTML data

--- emailcontents.py
+++ emailcontents.py
@@ -4,7 +4,7 @@ t = html(
     head(),
     body(
     """Hello\n My name is Abdullah Hijazi with Universal Meditech Incorporated. \n\n
-        We're a manufacturer of High Quality covid-19 antigen tests. Product infomration is attachment as SARS-COV-2 Antigen Rapid Test Colloidal Gold Kit.  Our tests are affordable, easy to use, and a positive result is granted in 2 minutes with 98% accuracy (detailed statistics in the attached product information).   Faster then PCR tests, our test is perfect for preventing an outbreak after a workplace exposure. \n \n 
+        We are a manufacturer of High Quality covid-19 antigen tests. Product infomration is attachment as SARS-COV-2 Antigen Rapid Test Colloidal Gold Kit.  Our tests are affordable, easy to use, and a positive result is granted in 2 minutes with 98% accuracy (detailed statistics in the attached product information).   Faster then PCR tests, our test is perfect for preventing an outbreak after a workplace exposure. \n \n 
 
         We are confident that our product will fulfill your organization’s needs. If you are interested in purchasing our tests, getting some free samples, or have any further inquiries, please contact me anytime.\n
 
@@ -18,5 +18,6 @@ t = html(
         img(src="www.hijazz.solutions/gif")
     )
     ) 
-x=t.render()
-print(x)
+
+email_contents = t.render()
+
