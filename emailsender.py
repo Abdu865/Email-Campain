ea8630e5990e0beb2b1c9c0675d757c027f5675b
Abdu865 <abdullahhijazi3@gmail.com>
2022-01-31 00:55:20 -0500

    updated sender to use the file emailcontents.py to make HTML data- wrong file added last commit

--- emailsender.py
+++ emailsender.py
@@ -4,6 +4,7 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from pyexcel_ods import get_data
 import pprint
+from emailcontents import email_contents
 
 #wb = get_data('/home/abdullahhijazi/Documents/TestEmails.ods')
 #pprint.pprint(wb) # "Sheet1" -  0,x is email   -- 1,x is recepitant name --2,x is custom msg
@@ -16,31 +17,9 @@ import pprint
 #for x in wb["Sheet1"][2]:
 #    if x == "":
 #        print("hit")
+print(email_contents)
 sender = "isthisausernameidontknow@gmail.com"
 receivers = ["abdullahhijaziUMI@gmail.com",]
-body_of_email_html = """\
-<html>
-    <head></head>
-    <body>
-        <p>
-        Hello [CN]<br>
-        My name is Abdullah Hijazi with Universal Meditech Incorporated. <br><br>
-        We're a manufacturer of High Quality covid-19 antigen tests. Product infomration is attachment as SARS-COV-2 Antigen Rapid Test Colloidal Gold Kit.  Our tests are affordable, easy to use, and a positive result is granted in 2 minutes with 98% accuracy (detailed statistics in the attached product information).   Faster then PCR tests, our test is perfect for preventing an outbreak after a workplace exposure. <br><br> 
-
-        We are confident that our product will fulfill your organization’s needs. If you are interested in purchasing our tests, getting some free samples, or have any further inquiries, please contact me anytime. <br> 
-
-        Thank you!<br><br>
-        Best regards,<br>
-        Abdullah Hijazi<br>
-        Sales Representative<br> 
-        Universal Meditech Inc.<br> 
-        1320 E. Fortune Ave #102. Fresno, CA 93725<br> 
-        Direct 304-751-5596
-        <img src="https://www.google-analytics.com/collect?v=1&tid=G-WRMN1RD8GH-1&cid=555&aip=1&t=event&ec=email&ea=open&dp=gif&dt=gif">
-        </p>
-    </body>
-</html> 
-"""
 
 path_to_file= "SARS-COV-2 Antigen Rapid Test Colloidal Gold Kit.pdf"#Attachment file
 
@@ -52,7 +31,7 @@ with open(path_to_file, "rb") as f:
     attach.add_header('Content-Disposition', 'attachment', filename=str("SARS-COV-2 Antigen Rapid Test Colloidal Gold Kit"))
     msg.attach(attach)
 
-msg.attach(MIMEText(body_of_email_html,'html'))
+msg.attach(MIMEText(email_contents,'html'))
 msg["subject"] = "test email 2" #email subject like  TODO:Customize to sender
 msg["From"] = sender
 msg["To"] = ','.join(receivers)
